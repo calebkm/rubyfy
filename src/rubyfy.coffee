02a71d3f53a2202e94cb96caad2a18decdac7b07
Caleb KM <calebkm@gmail.com>
2019-11-12 12:38:52 -0800

    Added Rubyfy

---
+++ src/rubyfy.coffee
@@ -0,0 +1,118 @@
+# Rubyfy - 0.1.0
+# 2019 Caleb Matthiesen
+# https://github.com/calebkm/rubyfy
+# ----------------------------------------------------
+@Rubyfy or= {}
+
+# Setup --------------------------------------------
+#
+Rubyfy.classes = 'Array Object String'.split(' ')
+Rubyfy.Array   = 'any compact is_empty first last'.split(' ')
+Rubyfy.String  = 'capitalize downcase is_blank'.split(' ')
+Rubyfy.Object  = 'any is_empty keys vals'.split(' ')
+
+# Array --------------------------------------------
+#
+Rubyfy.compact = (arr) ->
+  if Rubyfy.has_func(arr, 'compact')
+    arr.filter (i) -> i != null and i != undefined
+
+Rubyfy.first = (arr) ->
+  if Rubyfy.has_func(arr, 'first')
+    arr[0]
+
+Rubyfy.last = (arr) ->
+  if Rubyfy.has_func(arr, 'last')
+    arr[arr.length - 1]
+
+# Object -------------------------------------------
+#
+Rubyfy.keys = (obj) ->
+  if Rubyfy.has_func(obj, 'keys')
+    Object.keys(obj)
+
+Rubyfy.vals = (obj) ->
+  if Rubyfy.has_func(obj, 'vals')
+    Object.values(obj)
+
+# Array or Object ----------------------------------
+#
+Rubyfy.any = (arr_or_obj) ->
+  if Rubyfy.has_func(arr_or_obj, 'any')
+    !Rubyfy.is_empty(arr_or_obj)
+
+Rubyfy.is_empty = (arr_or_obj) ->
+  if Rubyfy.has_func(arr_or_obj, 'is_empty')
+    if Rubyfy.is_array(arr_or_obj)
+      Rubyfy.compact(arr_or_obj).length == 0
+    else if Rubyfy.is_object(arr_or_obj)
+      Object.keys(arr_or_obj).length == 0
+
+# String -------------------------------------------
+#
+Rubyfy.capitalize = (str) ->
+  if Rubyfy.has_func(str, 'capitalize')
+    str.charAt(0).toUpperCase() + str.slice(1).toLowerCase()
+
+Rubyfy.downcase = (str) ->
+  if Rubyfy.has_func(str, 'downcase')
+    str.toLowerCase()
+
+Rubyfy.is_blank = (str) ->
+  if Rubyfy.has_func(str, 'is_blank')
+    str.trim() == ''
+
+# Helpers ------------------------------------------
+#
+Rubyfy.has_func = (o, func) ->
+  class_of = Rubyfy.class_of(o)
+  if class_of and Rubyfy[class_of] and Rubyfy[class_of].includes(func)
+    true
+  else
+    console.warn("Whoops, Rubyfy does not define a function `#{func}` on the #{Rubyfy.capitalize(class_of)} class.")
+
+Rubyfy.class_of = (t) ->
+  if Rubyfy.is_array(t)
+    'Array'
+  else if Rubyfy.is_string(t)
+    'String'
+  else if Rubyfy.is_object(t)
+    'Object'
+  else
+    typeof t
+
+Rubyfy.is_array = (a) ->
+  Array.isArray(a)
+
+Rubyfy.is_string = (s) ->
+   typeof s == 'string' or \
+   s.constructor == String or \
+   Object.prototype.toString.call(s) == '[object String]'
+
+Rubyfy.is_object = (o) ->
+  (typeof o == 'object') and \
+  (o != null) and \
+  !Rubyfy.is_array(o) and \
+  !Rubyfy.is_string(o)
+
+# Define `R` shorthand
+#-----------------------------------------------------
+unless Rubyfy.no_r
+  if typeof @R == 'undefined'
+    @R = Rubyfy
+    @_R_IS_DEFINED_ = true
+  else
+    console.warn("Unable to assign Rubyfy shorthand `R` because it's already defined. You can still use `Rubyfy` explicitly.")
+
+# Add Rubyfy to prototypes
+# ----------------------------------------------------
+unless Rubyfy.no_prototypes
+  for klass in Rubyfy.classes
+    do (klass) ->
+      for func in Rubyfy[klass]
+        do (func) ->
+          if typeof eval(klass).prototype[func] == 'undefined'
+            Object.defineProperty eval(klass).prototype, func,
+              value: -> Rubyfy[func](@)
+          else
+            console.warn("Rubyfy attempted to define `#{klass}##{func}` but it looks like #{klass}.prototype.#{func} has already been defined. You can still use the Rubyfy version of `#{func}` by calling it explicitly with `#{if @_R_IS_DEFINED_ then 'R' else 'Rubyfy'}.#{func}()`")
---
+++ src/rubyfy.js
@@ -0,0 +1,129 @@
+/*
+ * This file was created by running 'rubyfy.coffee' through decafinate
+ * https://github.com/decaffeinate/decaffeinate
+ */
+if (!this.Rubyfy) { this.Rubyfy = {}; }
+
+Rubyfy.classes = 'Array Object String'.split(' ');
+Rubyfy.Array   = 'any compact is_empty first last'.split(' ');
+Rubyfy.String  = 'capitalize downcase is_blank'.split(' ');
+Rubyfy.Object  = 'any is_empty keys vals'.split(' ');
+
+Rubyfy.compact = function(arr) {
+  if (Rubyfy.has_func(arr, 'compact')) {
+    return arr.filter(i => (i !== null) && (i !== undefined));
+  }
+};
+
+Rubyfy.first = function(arr) {
+  if (Rubyfy.has_func(arr, 'first')) {
+    return arr[0];
+  }
+};
+
+Rubyfy.last = function(arr) {
+  if (Rubyfy.has_func(arr, 'last')) {
+    return arr[arr.length - 1];
+  }
+};
+
+Rubyfy.keys = function(obj) {
+  if (Rubyfy.has_func(obj, 'keys')) {
+    return Object.keys(obj);
+  }
+};
+
+Rubyfy.vals = function(obj) {
+  if (Rubyfy.has_func(obj, 'vals')) {
+    return Object.values(obj);
+  }
+};
+
+Rubyfy.any = function(arr_or_obj) {
+  if (Rubyfy.has_func(arr_or_obj, 'any')) {
+    return !Rubyfy.is_empty(arr_or_obj);
+  }
+};
+
+Rubyfy.is_empty = function(arr_or_obj) {
+  if (Rubyfy.has_func(arr_or_obj, 'is_empty')) {
+    if (Rubyfy.is_array(arr_or_obj)) {
+      return Rubyfy.compact(arr_or_obj).length === 0;
+    } else if (Rubyfy.is_object(arr_or_obj)) {
+      return Object.keys(arr_or_obj).length === 0;
+    }
+  }
+};
+
+Rubyfy.capitalize = function(str) {
+  if (Rubyfy.has_func(str, 'capitalize')) {
+    return str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
+  }
+};
+
+Rubyfy.downcase = function(str) {
+  if (Rubyfy.has_func(str, 'downcase')) {
+    return str.toLowerCase();
+  }
+};
+
+Rubyfy.is_blank = function(str) {
+  if (Rubyfy.has_func(str, 'is_blank')) {
+    return str.trim() === '';
+  }
+};
+
+Rubyfy.has_func = function(o, func) {
+  const class_of = Rubyfy.class_of(o);
+  if (class_of && Rubyfy[class_of] && Rubyfy[class_of].includes(func)) {
+    return true;
+  } else {
+    return console.warn(`Whoops, Rubyfy does not define a function \`${func}\` on the ${Rubyfy.capitalize(class_of)} class.`);
+  }
+};
+
+Rubyfy.class_of = function(t) {
+  if (Rubyfy.is_array(t)) {
+    return 'Array';
+  } else if (Rubyfy.is_string(t)) {
+    return 'String';
+  } else if (Rubyfy.is_object(t)) {
+    return 'Object';
+  } else {
+    return typeof t;
+  }
+};
+
+Rubyfy.is_array = a => Array.isArray(a);
+
+Rubyfy.is_string = s => (typeof s === 'string') || 
+(s.constructor === String) || 
+(Object.prototype.toString.call(s) === '[object String]');
+
+Rubyfy.is_object = o => (typeof o === 'object') && 
+(o !== null) && 
+!Rubyfy.is_array(o) && 
+!Rubyfy.is_string(o);
+
+if (!Rubyfy.no_r) {
+  if (typeof this.R === 'undefined') {
+    this.R = Rubyfy;
+    this._R_IS_DEFINED_ = true;
+  } else {
+    console.warn("Unable to assign Rubyfy shorthand `R` because it's already defined. You can still use `Rubyfy` explicitly.");
+  }
+}
+
+if (!Rubyfy.no_prototypes) {
+  for (let klass of Array.from(Rubyfy.classes)) {
+    ((klass => Array.from(Rubyfy[klass]).map((func) =>
+      (function(func) {
+        if (typeof eval(klass).prototype[func] === 'undefined') {
+          return Object.defineProperty(eval(klass).prototype, func,
+            {value() { return Rubyfy[func](this); }});
+        } else {
+          return console.warn(`Rubyfy attempted to define \`${klass}#${func}\` but it looks like ${klass}.prototype.${func} has already been defined. You can still use the Rubyfy version of \`${func}\` by calling it explicitly with \`${this._R_IS_DEFINED_ ? 'R' : 'Rubyfy'}.${func}()\``);
+        }
+      })(func))))(klass);
+  }
+}
